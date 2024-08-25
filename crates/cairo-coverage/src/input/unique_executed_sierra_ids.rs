ec81ef1de781d8f7b19b27e86134a130be621b25
Karol Sewilo <karol.sewilo@swmansion.com>
2024-08-25 23:47:33 +0200

    Add squashing of sierra_ids
    commit-id:aa709ff1

--- crates/cairo-coverage/src/input/unique_executed_sierra_ids.rs
+++ crates/cairo-coverage/src/input/unique_executed_sierra_ids.rs
@@ -1,3 +1,4 @@
+use crate::input::SierraToCairoMap;
 use anyhow::{Context, Result};
 use cairo_lang_sierra::program::StatementIdx;
 use cairo_lang_sierra_to_casm::compiler::CairoProgram;
@@ -10,7 +11,11 @@ use trace_data::{CallTrace, CasmLevelInfo};
 pub struct UniqueExecutedSierraIds(HashMap<StatementIdx, usize>);
 
 impl UniqueExecutedSierraIds {
-    pub fn new(casm: &CairoProgram, call_trace: &CallTrace) -> Result<Self> {
+    pub fn new(
+        casm: &CairoProgram,
+        call_trace: &CallTrace,
+        sierra_to_cairo_map: &SierraToCairoMap,
+    ) -> Result<Self> {
         let CasmLevelInfo {
             run_with_call_header,
             vm_trace,
@@ -24,22 +29,48 @@ impl UniqueExecutedSierraIds {
             .then(|| vm_trace.last().map_or(1, |trace| trace.pc + 1))
             .unwrap_or(1);
 
-        Ok(Self(
-            vm_trace
-                .iter()
-                .map(|step| step.pc)
-                .filter(|pc| pc >= &real_minimal_pc)
-                .map(|pc| {
-                    let real_pc_code_offset = pc - real_minimal_pc;
-                    casm.debug_info
-                        .sierra_statement_info
-                        .partition_point(|debug_info| {
-                            debug_info.start_offset <= real_pc_code_offset
-                        })
-                        - 1
-                })
-                .map(StatementIdx)
-                .counts(),
+        let iter = vm_trace
+            .iter()
+            .map(|step| step.pc)
+            .filter(|pc| pc >= &real_minimal_pc)
+            .map(|pc| {
+                let real_pc_code_offset = pc - real_minimal_pc;
+                casm.debug_info
+                    .sierra_statement_info
+                    .partition_point(|debug_info| debug_info.start_offset <= real_pc_code_offset)
+                    - 1
+            })
+            .map(StatementIdx);
+
+        Ok(squash_idx_pointing_to_same_statement(
+            iter,
+            sierra_to_cairo_map,
         ))
     }
+}
+
+fn squash_idx_pointing_to_same_statement<I: Iterator<Item = StatementIdx>>(
+    iter: I,
+    sierra_to_cairo_map: &SierraToCairoMap,
+) -> UniqueExecutedSierraIds {
+    UniqueExecutedSierraIds(
+        iter.fold(Vec::new(), |mut acc, statement_idx| {
+            if points_to_different_statement(sierra_to_cairo_map, acc.last(), statement_idx) {
+                acc.push(statement_idx);
+            }
+            acc
+        })
+        .into_iter()
+        .counts(),
+    )
+}
+
+fn points_to_different_statement(
+    sierra_to_cairo_map: &SierraToCairoMap,
+    last_idx: Option<&StatementIdx>,
+    current_idx: StatementIdx,
+) -> bool {
+    last_idx.map_or(true, |last_idx| {
+        sierra_to_cairo_map.get(last_idx) != sierra_to_cairo_map.get(&current_idx)
+    })
 }
