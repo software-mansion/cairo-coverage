d52c552fd5db2491aec6b9156ae8bfa53a4a0855
ksew1 <95349104+ksew1@users.noreply.github.com>
2024-10-16 04:49:41 -0700

    Use cairo_annotations Coverage annotations (#78)
    **Stack**:
    - #78 ⬅
    - #77
    - #76
    - #75
    
    
    ⚠️ *Part of a stack created by [spr](https://github.com/ejoffe/spr). Do
    not merge manually using the UI - doing so may have unexpected results.*

--- crates/cairo-coverage/src/input/sierra_to_cairo_map.rs
+++ crates/cairo-coverage/src/input/sierra_to_cairo_map.rs
@@ -1,17 +1,21 @@
-use crate::data_loader::{CodeLocation, CoverageAnnotations, LineRange};
 use crate::input::statement_category_filter::{StatementCategoryFilter, VIRTUAL_FILE_REGEX};
-use crate::types::FileLocation;
 use anyhow::{Context, Result};
+use cairo_annotations::annotations::coverage::{
+    CodeLocation, CoverageAnnotationsV1, LineNumber, SourceCodeSpan, SourceFileFullPath,
+    VersionedCoverageAnnotations,
+};
 use cairo_annotations::annotations::profiler::{
     FunctionName, ProfilerAnnotationsV1, VersionedProfilerAnnotations,
 };
 use cairo_annotations::annotations::TryFromDebugInfo;
-use cairo_lang_sierra::debug_info::{Annotations, DebugInfo};
+use cairo_lang_sierra::debug_info::DebugInfo;
 use cairo_lang_sierra::program::StatementIdx;
 use derived_deref::Deref;
-use indoc::{formatdoc, indoc};
-use serde::de::DeserializeOwned;
+use indoc::indoc;
+use serde::Deserialize;
 use std::collections::HashMap;
+use std::iter;
+use std::ops::RangeInclusive;
 
 const RECOMMENDED_CAIRO_PROFILE_TOML: &str = indoc! {
     r#"
@@ -30,15 +34,44 @@ pub struct SierraToCairoMap(HashMap<StatementIdx, StatementOrigin>);
 #[derive(Clone, Eq, PartialEq)]
 pub struct StatementOrigin {
     pub function_name: FunctionName,
-    pub file_location: FileLocation,
+    pub source_file_full_path: SourceFileFullPath,
     pub line_range: LineRange,
 }
 
 impl StatementOrigin {
     pub fn remove_virtual_file_prefix(&mut self) {
-        self.file_location = VIRTUAL_FILE_REGEX
-            .replace_all(&self.file_location, "")
-            .to_string();
+        self.source_file_full_path = SourceFileFullPath(
+            VIRTUAL_FILE_REGEX
+                .replace_all(&self.source_file_full_path.0, "")
+                .to_string(),
+        );
+    }
+}
+
+#[derive(Deserialize, Clone, Eq, PartialEq)]
+pub struct LineRange {
+    /// Line number is 1-based
+    pub start: LineNumber,
+    /// Line number is 1-based
+    pub end: LineNumber,
+}
+
+impl From<&SourceCodeSpan> for LineRange {
+    fn from(span: &SourceCodeSpan) -> Self {
+        // `SourceCodeSpan` is 0-based, so we need to add 1 to the line numbers
+        Self {
+            start: span.start.line + LineNumber(1),
+            end: span.end.line + LineNumber(1),
+        }
+    }
+}
+
+impl IntoIterator for &LineRange {
+    type Item = LineNumber;
+    type IntoIter = iter::Map<RangeInclusive<usize>, fn(usize) -> LineNumber>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        (self.start.0..=self.end.0).map(LineNumber)
     }
 }
 
@@ -46,9 +79,10 @@ pub fn create_sierra_to_cairo_map(
     debug_info: &DebugInfo,
     filter: &StatementCategoryFilter,
 ) -> Result<SierraToCairoMap> {
-    let CoverageAnnotations {
+    let VersionedCoverageAnnotations::V1(CoverageAnnotationsV1 {
         statements_code_locations,
-    } = CoverageAnnotations::get_namespace(&debug_info.annotations)?;
+    }) = VersionedCoverageAnnotations::try_from_debug_info(debug_info)
+        .context(RECOMMENDED_CAIRO_PROFILE_TOML)?;
 
     let VersionedProfilerAnnotations::V1(ProfilerAnnotationsV1 {
         statements_functions,
@@ -79,10 +113,10 @@ fn find_statement_origin(
         .iter()
         .zip(function_names)
         .map(
-            |((file_location, line_range), function_name)| StatementOrigin {
+            |(CodeLocation(source_file_full_path, line_range), function_name)| StatementOrigin {
                 function_name: function_name.clone(),
-                file_location: file_location.clone(),
-                line_range: line_range.move_by_1(),
+                source_file_full_path: source_file_full_path.clone(),
+                line_range: line_range.into(),
             },
         )
         .find(|statement_origin| filter.should_include(statement_origin))
@@ -90,34 +124,4 @@ fn find_statement_origin(
             statement_origin.remove_virtual_file_prefix();
             statement_origin
         })
-}
-
-trait Namespace {
-    const NAMESPACE: &'static str;
-
-    fn get_namespace<T: DeserializeOwned>(annotations: &Annotations) -> Result<T> {
-        annotations
-            .get(Self::NAMESPACE)
-            .cloned()
-            .context(formatdoc! {
-                r#"Expected key: {} but was missing.
-
-                Perhaps you are missing the following entries in Scarb.toml:
-
-                [profile.dev.cairo]
-                unstable-add-statements-functions-debug-info = true
-                unstable-add-statements-code-locations-debug-info = true
-                inlining-strategy= "avoid"
-                "#,
-                Self::NAMESPACE,
-            })
-            .and_then(|value| {
-                serde_json::from_value(value)
-                    .context(format!("Failed to deserialize at key: {}", Self::NAMESPACE))
-            })
-    }
-}
-
-impl Namespace for CoverageAnnotations {
-    const NAMESPACE: &'static str = "github.com/software-mansion/cairo-coverage";
 }
