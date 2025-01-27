de682335bffef49095480a618f66ec1c7f53a714
Karol Sewilo <karol.sewilo@swmansion.com>
2025-01-27 10:31:43 +0100

    Use scarb metadata
    commit-id:7b5072f8

--- crates/cairo-coverage-core/benches/benchmarks/mod.rs
+++ crates/cairo-coverage-core/benches/benchmarks/mod.rs
@@ -1,3 +1,4 @@
+use cairo_coverage_core::args::RunOptions;
 use cairo_coverage_test_utils::read_files_from_dir;
 use camino::Utf8PathBuf;
 use criterion::Criterion;
@@ -8,6 +9,19 @@ pub mod starknet_staking;
 /// Wrapper around `read_files_from_dir` to make it easier to use in benchmarks.
 fn trace_files_for_benches(dir_name: &str) -> Vec<Utf8PathBuf> {
     read_files_from_dir(format!("benches/project-traces/{dir_name}/trace"))
+}
+
+/// Create [`RunOptions`] with set `project_path` and empty `include`.
+/// If we leave the `project_path` as `None`, the `scarb_metadata` will fail.
+fn run_options(dir_name: &str) -> RunOptions {
+    let project_path = Utf8PathBuf::from(format!("benches/project-traces/{dir_name}"))
+        .canonicalize_utf8()
+        .unwrap()
+        .into();
+    RunOptions {
+        include: Vec::default(),
+        project_path,
+    }
 }
 
 /// Config of [`Criterion`] that should be used for all benchmarks.
--- crates/cairo-coverage-core/benches/benchmarks/starknet_staking.rs
+++ crates/cairo-coverage-core/benches/benchmarks/starknet_staking.rs
@@ -1,14 +1,15 @@
-use crate::benchmarks::{config, trace_files_for_benches};
-use cairo_coverage_core::args::RunOptions;
+use crate::benchmarks::{config, run_options, trace_files_for_benches};
 use criterion::{criterion_group, Criterion};
 use std::hint::black_box;
+
+const PROJECT_NAME: &str = "starknet-staking";
 
 /// Benchmark of [`starknet_staking`](https://github.com/starkware-libs/starknet-staking) with default options.
 /// The trace files should be generated using `download_bench_project.sh` script.
 fn starknet_staking_benchmark(c: &mut Criterion) {
-    let trace_files = trace_files_for_benches("starknet-staking");
-    let run_options = RunOptions::default();
-    c.bench_function("starknet-staking default options", |b| {
+    let trace_files = trace_files_for_benches(PROJECT_NAME);
+    let run_options = run_options(PROJECT_NAME);
+    c.bench_function("starknet-staking", |b| {
         b.iter(|| {
             cairo_coverage_core::run(
                 black_box(trace_files.clone()),
