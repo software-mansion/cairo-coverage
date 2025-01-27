c0b7357ea18754bae34d399aca9c2cec8ee7735e
Karol Sewilo <karol.sewilo@swmansion.com>
2025-01-27 10:43:10 +0100

    Introduce load module
    commit-id:88f23cf2

--- crates/cairo-coverage-core/src/data_loader/mod.rs
+++
@@ -1,4 +0,0 @@
-mod loaded_data;
-mod sierra_program;
-
-pub use loaded_data::{LoadedData, LoadedDataMap};
--- crates/cairo-coverage-core/src/input/data.rs
+++ crates/cairo-coverage-core/src/input/data.rs
@@ -1,8 +1,12 @@
-use crate::data_loader::LoadedData;
 use crate::input::filter::statement_category_filter::StatementCategoryFilter;
 use crate::input::{create_sierra_to_cairo_map, SierraToCairoMap, UniqueExecutedSierraIds};
+use crate::loading::enriched_program::EnrichedProgram;
+use crate::loading::execution_data::ExecutionData;
 use crate::merge::MergeOwned;
 use anyhow::{Context, Result};
+use cairo_lang_sierra::program::Program;
+use cairo_lang_sierra_to_casm::compiler::{CairoProgramDebugInfo, SierraToCasmConfig};
+use cairo_lang_sierra_to_casm::metadata::{calc_metadata, MetadataComputationConfig};
 
 pub struct InputData {
     pub unique_executed_sierra_ids: UniqueExecutedSierraIds,
@@ -11,19 +15,33 @@ pub struct InputData {
 
 impl InputData {
     pub fn new(
-        LoadedData {
-            debug_info,
+        ExecutionData {
             casm_level_infos,
-            casm_debug_info,
-            program,
-        }: &LoadedData,
+            enriched_program:
+                EnrichedProgram {
+                    program,
+                    coverage_annotations,
+                    profiler_annotations,
+                    ..
+                },
+        }: ExecutionData,
         filter: &StatementCategoryFilter,
     ) -> Result<Self> {
-        let sierra_to_cairo_map = create_sierra_to_cairo_map(debug_info, filter, program)?;
+        let casm_debug_info = compile(&program).expect("Failed to compile program to casm");
+        let sierra_to_cairo_map = create_sierra_to_cairo_map(
+            coverage_annotations,
+            profiler_annotations,
+            filter,
+            &program,
+        );
         let unique_executed_sierra_ids = casm_level_infos
             .iter()
             .map(|casm_level_info| {
-                UniqueExecutedSierraIds::new(casm_debug_info, casm_level_info, &sierra_to_cairo_map)
+                UniqueExecutedSierraIds::new(
+                    &casm_debug_info,
+                    casm_level_info,
+                    &sierra_to_cairo_map,
+                )
             })
             .reduce(MergeOwned::merge_owned)
             .context("Failed to create unique executed sierra ids")?;
@@ -33,4 +51,16 @@ impl InputData {
             sierra_to_cairo_map,
         })
     }
+}
+
+fn compile(program: &Program) -> Result<CairoProgramDebugInfo> {
+    let casm = cairo_lang_sierra_to_casm::compiler::compile(
+        program,
+        &calc_metadata(program, MetadataComputationConfig::default())?,
+        SierraToCasmConfig {
+            gas_usage_check: false,
+            max_bytecode_size: usize::MAX,
+        },
+    )?;
+    Ok(casm.debug_info)
 }
